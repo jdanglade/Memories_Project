d70542056505dbb717434192bb749d35b61be3b4
Justin Danglade <just.danglade@gmail.com>
2020-11-26 14:52:08 -0500

    setting up redux actions

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import { useDispatch } from "react-redux";
+import { getPosts } from "./actions/posts";
 import memories from "./images/memories.png";
 import Form from "./components/Forms/Form.js";
 import Posts from "./components/Posts/Posts.js";
@@ -7,6 +9,12 @@ import useStyles from "./styles";
 
 const App = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
+
   return (
     <Container maxWidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
