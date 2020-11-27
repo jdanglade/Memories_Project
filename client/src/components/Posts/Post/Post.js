54cc49b28225f2b19afc5e93477221c520d499ac
Justin Danglade <just.danglade@gmail.com>
2020-11-27 17:40:11 -0500

    Update Post.js
    setting up 'Posts'

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,6 +1,19 @@
 import React from "react";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@material-ui/core";
+
+import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
+import useStyles from "./styles";
 
 const Post = () => {
+  const classes = useStyles();
+
   return <h1>POST</h1>;
 };
 
