2567d74f1f6b0a1657fc8e587758ded0978442ef
Justin Danglade <just.danglade@gmail.com>
2020-12-01 17:00:39 -0500

    more material icons

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -9,6 +9,8 @@ import {
 } from "@material-ui/core";
 
 import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
+import DeleteIcon from "@material-ui/icons/Delete";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import useStyles from "./styles";
 
 const Post = () => {
