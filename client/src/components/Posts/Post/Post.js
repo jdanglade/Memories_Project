915dc51cb9b1bcb1a9b59ef812c8061d5e942f6e
Justin Danglade <just.danglade@gmail.com>
2020-12-05 14:52:11 -0500

    update material-ui dependencies

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -11,12 +11,29 @@ import {
 import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
 import DeleteIcon from "@material-ui/icons/Delete";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import moment from "moment";
 import useStyles from "./styles";
 
-const Post = () => {
+const Post = ({ post }) => {
   const classes = useStyles();
-
-  return <h1>POST</h1>;
+  return (
+    <Card className={classes.card}>
+      <CardMedia
+        className={classes.media}
+        image={post.selectedStyle}
+        title={post.title}
+      />
+      <div className={classes.overlay}>
+        <Typography variant="h6">{post.creator}</Typography>
+        <Typography variant="body2">
+          {moment(post.createdAt).fromNow()}
+        </Typography>
+      </div>
+      <div className={classes.overlay2}>
+        <Button style={{ color: "white" }} size="small"></Button>
+      </div>
+    </Card>
+  );
 };
 
 export default Post;
