55bee8c7097c77b809d135dd11f3319d4d9bb44d
Justin Danglade <just.danglade@gmail.com>
2020-12-12 10:24:38 -0500

    Grid spacing fix - {} not ""

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -15,7 +15,7 @@ const Posts = ({ setCurrentId }) => {
       className={classes.container}
       container
       alignItems="stretch"
-      spacing="3"
+      spacing={3}
     >
       {posts.map((post) => (
         <Grid key={post._id} item xs={12} sm={6}>
