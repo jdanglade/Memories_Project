a19712e2f6c486f8cbf65fadf41ad17448763d8a
Justin Danglade <just.danglade@gmail.com>
2020-12-16 01:10:58 -0500

    working on delete

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -47,7 +47,7 @@ export const deletePost = async (req, res) => {
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(404).send("No post with that id");
   }
-  await PostMessage.findByIdAndDelete(id);
+  await PostMessage.findByIdAndRemove(id);
   console.log(`DELETE`);
-  res.json("Post message successfully deleted");
+  res.json({ messsage: "Post message successfully deleted" });
 };
